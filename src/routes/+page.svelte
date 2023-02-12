8c6aaddf01204cdd48a9bb9706f7096763997e29
DevNdy <developpeur.ndy@gmail.com>
2023-02-12 14:47:32 +0100

    F01 feat LOGIN > Create loginPage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<div>
+		<h1>Bienvenue sur Jhémis <span>le réseau social de la justice</span></h1>
+	</div>
+	<img
+		src="https://images.pexels.com/photos/6077123/pexels-photo-6077123.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+		alt="justice"
+	/>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	div {
+		width: 100%;
+	}
+
+	h1 {
+		margin-top: 100px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		font-size: 45px;
+	}
+
+	span {
+		font-weight: 200;
+		font-size: 24px;
+		color: rgb(98, 98, 98);
+	}
+
+	img {
+		height: 100vh;
+		filter: grayscale(60%);
+	}
+</style>
